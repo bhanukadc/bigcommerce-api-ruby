f9af6312fb451d050581cddb2464984e165b1827
ParthivSavani <parthiv.savani@gmail.com>
2018-10-09 09:12:12 +0530

    Integrate new API to support multicurrency

---
+++ lib/bigcommerce/resources/store/currency.rb
@@ -0,0 +1,22 @@
+# Currency
+# https://developer.bigcommerce.com/api/stores/v2/products
+
+module Bigcommerce
+  class Currency < Resource
+    include Bigcommerce::ResourceActions.new uri: 'currencies/%d'
+
+    property :id
+    property :is_default
+    property :country_iso2
+    property :currency_code
+    property :currency_exchange_rate
+    property :token
+    property :decimal_token
+    property :thousands_token
+    property :decimal_places
+
+    def self.count(params = {})
+      get 'currencies/count', params
+    end
+  end
+end
